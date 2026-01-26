0c17121780eb7312a7f8a086001b2950ff8504a0
Roman Strakhov <roman@bestsiteever.net>
2026-01-26 20:47:50 +0400

    feat: Update shared color palette with adjusted hex codes for Red, Purple, and Pink

--- shared/colors.js
+++ shared/colors.js
@@ -7,14 +7,14 @@
  */
 
 const COLORS = [
-  { index: 0, hex: '#eb0c0c', name: 'Red' },
+  { index: 0, hex: '#d40808', name: 'Red' },
   { index: 1, hex: '#0D47A1', name: 'Blue' },
   { index: 2, hex: '#43A047', name: 'Green' },
   { index: 3, hex: '#FDD835', name: 'Yellow' },
   { index: 4, hex: '#FB8C00', name: 'Orange' },
-  { index: 5, hex: '#8E24AA', name: 'Purple' },
+  { index: 5, hex: '#75118f', name: 'Purple' },
   { index: 6, hex: '#00CED1', name: 'Aqua' },
-  { index: 7, hex: '#9e375a', name: 'Maroon' },
+  { index: 7, hex: '#ff19d1', name: 'Pink' },
   { index: 8, hex: '#6D4C41', name: 'Brown' },
   { index: 9, hex: '#616161', name: 'Dark Grey' },
 ];
